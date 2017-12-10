7242852fa16a3d63e3423f15c8d8ed73727d0a13
Sean Andersen <smandersen13@gmail.com>
2017-12-10 10:29:57 -0500

    additonal js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var pickfirstchar = true;
   var picksecondchar = false;
 
-  console.log('first char', pickfirstchar);
+  console.log('should be true for first pick', pickfirstchar);
 
   ///hardcode all below properties into character objects
 
@@ -49,144 +49,214 @@ $(document).ready(function() {
     img: "<img src='./assets/images/emperorpalp.jpg' class='test'>"
   };
 
+  var hancounter = 0
+  var obicounter = 0
+  var yodacounter = 0
+  var bobacounter = 0
+  var darthcounter = 0
+  var empcounter = 0
 
-  var hansolo = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon han'>").append($("<div class='card-header iconfix1'>Han Solo</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + han.health + "</p>").append($("<div class='card-body iconfix2'>").append($(han.img)))))));
+
+  var hansolo = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon han'>").append($("<div class='card-header rebelcharactericon'>Han Solo</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + han.health + "</p>").append($("<div class='card-body iconfix2'>").append($(han.img)))))));
 
 
-  var obiwan = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon obi'>").append($("<div class='card-header iconfix1'>Obi-Wan</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + obi.health + "</p>").append($("<div class='card-body iconfix2'>").append($(obi.img)))))));
+  var obiwan = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon obi'>").append($("<div class='card-header rebelcharactericon'>Obi-Wan</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + obi.health + "</p>").append($("<div class='card-body iconfix2'>").append($(obi.img)))))));
 
-  var yodah = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon yoda'>").append($("<div class='card-header iconfix1'>Yoda</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + yoda.health + "</p>").append($("<div class='card-body iconfix2'>").append($(yoda.img)))))));
+  var yodah = ($("<div class='card text-white bg-primary mb-3 iconfix3 rebelicon yoda'>").append($("<div class='card-header rebelcharactericon'>Yoda</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + yoda.health + "</p>").append($("<div class='card-body iconfix2'>").append($(yoda.img)))))));
 
-  var bobafett = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon boba'>").append($("<div class='card-header iconfix1'>Boba Fett</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health: " + boba.health + "</p>").append($("<div class='card-body iconfix2'>").append($(boba.img)))))));
+  var bobafett = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon boba'>").append($("<div class='card-header empirecharactericon'>Boba Fett</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health: " + boba.health + "</p>").append($("<div class='card-body iconfix2'>").append($(boba.img)))))));
 
-  var darthvader = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon darth'>").append($("<div class='card-header iconfix1'>Darth Vader</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + darth.health + "</p>").append($("<div class='card-body iconfix2'>").append($(darth.img)))))));
+  var darthvader = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon darth'>").append($("<div class='card-header empirecharactericon'>Darth Vader</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + darth.health + "</p>").append($("<div class='card-body iconfix2'>").append($(darth.img)))))));
 
-  var emperor = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon emp'>").append($("<div class='card-header iconfix1'>Emperor</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + emp.health + "</p>").append($("<div class='card-body iconfix2'>").append($(emp.img)))))));
+  var emperor = ($("<div class='card text-white bg-danger mb-3 iconfix3 empireicon emp'>").append($("<div class='card-header empirecharactericon'>Emperor</div>").append($("<div class='card-body iconfix2'>").append($("<p class='card-text'>Health:" + emp.health + "</p>").append($("<div class='card-body iconfix2'>").append($(emp.img)))))));
 
-  function initiateGame() {
-      pickfirstchar=true
+function initiateGame() {
 
-      if (pickfirstchar === true) {
+      if (pickfirstchar === true && picksecondchar === false) {
 
-          console.log("TESTWORK",pickfirstchar)
-          console.log($(".gamesettings"))
+          $(".firstpick").css("border", "solid 5px #ffc107");
 
+
+          /*
           $(".gamesettings").text("PICK YOUR CHARACTER TO FIGHT IN THE ARENA");  //Make this blink so the person sees it more clearly
-
+          */
+ function hanfirst() {
           $(".han").on("click", function() {
               $("#arena1").append($(hansolo))
               $("#rebels").empty()
+              hancounter++
               pickfirstchar = false
               picksecondchar = true
-              pickempiresecond();
-              console.log(".hanclick",pickfirstchar)
-
+              console.log('if han first picked, second pick should be true',picksecondchar)
+              console.log('if han is first picked what is counter',hancounter)
+              pickempiresecond()
           });
+ }
+ function obifirst() {
           $(".obi").on("click", function() {
               $("#arena1").append($(obiwan))
               $("#rebels").empty()
               pickfirstchar = false
               picksecondchar = true
+              obicounter++
+              console.log('if obi first picked, second pick should be true',picksecondchar)
+              console.log('if obi is first picked what is counter',obicounter)
               pickempiresecond()
           });
+ }
+ function yodafirst() {
           $(".yoda").on("click", function() {
               $("#arena1").append($(yodah))
               $("#rebels").empty()
+              yodacounter++
               pickfirstchar = false
               picksecondchar = true
+              console.log('if yoda first picked, second pick should be true',picksecondchar)
+              console.log('if yoda is first picked what is counter',yodacounter)
               pickempiresecond()
           });
+ }
+ function bobafirst() {
           $(".boba").on("click", function() {
               $("#arena1").append($(bobafett))
               $("#empire").empty()
               pickfirstchar = false
               picksecondchar = true
+              bobacounter++
+              console.log('if boba first picked, second pick should be true',picksecondchar)
+              console.log('if boba is first picked what is counter',bobacounter)
               pickrebelsecond()
           });
+ }
+ function darthfirst () {
           $(".darth").on("click", function() {
               $("#arena1").append($(darthvader))
               $("#empire").empty()
               pickfirstchar = false
               picksecondchar = true
+              darthcounter++
+              console.log('if darth first picked, second pick should be true',picksecondchar)
+              console.log('if darth is first picked what is counter',darthcounter)
               pickrebelsecond()
           });
+ }
+ function empfirst() {
           $(".emp").on("click", function() {
               $("#arena1").append($(emperor))
               $("#empire").empty()
               pickfirstchar = false
               picksecondchar = true
+              empcounter++
+              console.log('if emperor first picked, second pick should be true',picksecondchar)
+              console.log('if emperor is first picked what is counter',empcounter)
               pickrebelsecond()
           });
-
-        }
+   }
   };
 
-  function pickrebelsecond() {
+function pickrebelsecond() {
 
-    if (picksecondchar === true) {
+    picksecondchar = true
+    pickfirstchar = false
 
+    if (picksecondchar === true && pickfirstchar === false) {
+
+    console.log('rebel second pick test',picksecondchar)
+
+    $(".firstpick").css("border", "");
+    $(".secondpick").css("border", "solid 5px #ffc107");
+
+
+    /*
     $(".gamesettings").text("PICK THE ENEMY CHARACTER TO FIGHT IN THE ARENA");//Make this blink so the person sees it more clearly
-
+    */
     $(".han").on("click", function() {
         $("#arena2").append($(hansolo))
         $("#rebels").append(obiwan,yodah);
         picksecondchar = false
+        hancounter++
         arenafight()
     });
     $(".obi").on("click", function() {
         $("#arena2").append($(obiwan))
         $("#rebels").append(hansolo,yodah);
         picksecondchar = false
+        obicounter++
         arenafight()
     });
     $(".yoda").on("click", function() {
         $("#arena2").append($(yodah))
         $("#rebels").append(hansolo,obiwan);
         picksecondchar = false
+        yodacounter++
+        console.log('yodacountersecond',yodacounter)
         arenafight()
     });
 
   }
   };
+function pickempiresecond() {
 
-  function pickempiresecond() {
-    if (picksecondchar === true) {
 
-      $(".gamesettings").text("PICK THE ENEMY CHARACTER TO FIGHT IN THE ARENA");
+    if (picksecondchar === true && pickfirstchar === false) {
+
+      console.log('empire second pick test',picksecondchar)
+
+      $(".firstpick").css("border", "");
+      $(".secondpick").css("border", "solid 5px #ffc107");
 
       $(".boba").on("click", function() {
           $("#arena2").append($(bobafett))
           $("#empire").append(darthvader,emperor);
           picksecondchar = false
+          bobacounter++
           arenafight()
       });
       $(".darth").on("click", function() {
           $("#arena2").append($(darthvader))
           $("#empire").append(bobafett,emperor);
           picksecondchar = false
+          darthcounter++
           arenafight()
       });
       $(".emp").on("click", function() {
           $("#arena2").append($(emperor))
           $("#empire").append(bobafett,darthvader);
           picksecondchar = false
+          empcounter++
+          console.log('if emperor is second picked, what is counter',empcounter)
           arenafight()
       });
 
     }
   }
-
-  function arenafight() {
+function arenafight() {
     if (pickfirstchar === false && picksecondchar === false) {
-      $(".gamesettings").text("PRESS THE ATTACK BUTTON TO BEGIN FIGHTING");
+
+      if (yodacounter === 1 && empcounter === 2 ) {
+
+          console.log('both characters picked',true)
+      }
+
+
+
+
+      $(".firstpick").css("border", "");
+      $(".secondpick").css("border", "");
+      $(".third-arena").css("border", "solid 5px #ffc107");
+
+
+
     }
   }
+
 
 
   $("#rebels").append(hansolo,obiwan,yodah);
   $("#empire").append(bobafett,darthvader,emperor);
 
-  initiateGame();
+
+initiateGame();
 });
 
 
